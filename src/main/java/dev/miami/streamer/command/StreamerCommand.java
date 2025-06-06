970f6dc166756a02aa6e71dd81e6a67a4e2956a3
Miami <ogunamenow@gmail.com>
2025-06-07 00:22:04 +0200

    yeah i hate this

--- src/main/java/dev/miami/streamer/command/StreamerCommand.java
+++ src/main/java/dev/miami/streamer/command/StreamerCommand.java
@@ -17,7 +17,7 @@ public class StreamerCommand {
                 )
                 .then(ClientCommandManager.literal("change")
                         .then(ClientCommandManager.literal("others")
-                                .then(ClientCommandManager.argument("prefix", StringArgumentType.greedyString())
+                                .then(ClientCommandManager.argument("username", StringArgumentType.greedyString())
                                         .executes(ctx -> Streamer.changeOthersName(StringArgumentType.getString(ctx, "prefix")))
                                 )
                         )
